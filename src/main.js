f2d83bc92684b76e7e85d759ed6535a88783598f
Reed-bit <78014863+Reed-bit@users.noreply.github.com>
2022-04-25 11:52:44 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function main() {
+  return (
+    <div>
+        <h2>Fun facts about React</h2>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by jordan walke</li>
+            <li>Has well over 100k starts on github</li>
+            <li>Its maintained byFacebook</li>
+            <li>Powers thousands of enteprise apps , including mobile apps</li>
+        </ul>
+    </div>
+  )
+}
+
+export default main
---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function navbar() {
+  return (
+    <nav>
+      <img src= {Logo} alt='logo' className='logoimg'/>
+      <h3 className='logotext'>ReactFacts</h3>
+      <h4 className='logotitle'>React course - project 1</h4>
+    </nav>
+  )
+}
+
+export default navbar
